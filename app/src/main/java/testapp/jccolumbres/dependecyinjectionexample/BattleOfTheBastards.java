cdc6f0c9e1978ef115f26a9e131dd90dbab46aae
Jigs <jhegs.knightdev@gmail.com>
2018-09-24 18:56:25 +0800

    Renamed Mainactivity

--- app/src/main/java/testapp/jccolumbres/dependecyinjectionexample/BattleOfTheBastards.java
+++ app/src/main/java/testapp/jccolumbres/dependecyinjectionexample/BattleOfTheBastards.java
@@ -3,11 +3,18 @@ package testapp.jccolumbres.dependecyinjectionexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class BattleOfTheBastards extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Starks starks = new Starks();
+        Boltons boltons = new Boltons();
+
+        War war = new War(starks,boltons);
+        war.prepare();
+        war.report();
     }
 }
